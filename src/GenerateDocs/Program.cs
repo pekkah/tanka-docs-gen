3dadffa46e8fd2617014f43c3d9ad077342080b7
Pekka Heikura <pekkah@gmail.com>
2018-12-14 12:58:21 +0200

    Initialize new project and setup as dotnet tool

---
+++ src/GenerateDocs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Fugu.GenerateDocs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
